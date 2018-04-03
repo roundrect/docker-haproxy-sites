b980e2dcebb5152d245f44e29557be586cb51663
johnscott <johnoscott@gmail.com>
2018-04-03 12:19:11 +1000

    changes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #
-docker-haproxy/
-docker-nginx-home/
-docker-nginx-preview/
-docker-shop/
-docker-shop-api/
-docker-stack-logging/
+# docker-haproxy/
+# docker-nginx-home/
+# docker-nginx-preview/
+# docker-shop/
+# docker-shop-api/
+# docker-stack-logging/
+
+git clone https://github.com/roundrect/docker-haproxy.git
+git clone https://github.com/roundrect/docker-nginx-home.git
+git clone https://github.com/roundrect/docker-nginx-preview.git
+git clone https://github.com/roundrect/docker-shop.git
+git clone https://github.com/roundrect/docker-shop-api.git
+git clone https://github.com/roundrect/docker-stack-logging.git
